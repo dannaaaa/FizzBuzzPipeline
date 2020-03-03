4f3d5d939bf080f81fe8b31423e8fe3138da91f5
Dana <danaawua@hotmail.co.uk>
2020-03-03 15:21:11 +0000

    first commit

---
+++ src/main/java/com/spartaglobal/FizzBussPipeline/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.FizzBussPipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
