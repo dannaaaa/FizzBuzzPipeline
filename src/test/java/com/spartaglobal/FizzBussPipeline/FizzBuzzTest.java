8794f9537be41ea5a66e7079727af2c066905c74
Jack Goodwin <jackgoodwin15@gmail.com>
2020-03-03 21:57:41 +0000

    Added two unit tests. POM now updated with JUnit 5

--- src/test/java/com/spartaglobal/FizzBussPipeline/FizzBuzzTest.java
+++ src/test/java/com/spartaglobal/FizzBussPipeline/FizzBuzzTest.java
@@ -1,20 +1,25 @@
 package com.spartaglobal.FizzBussPipeline;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import java.util.List;
 
-/**
- * Unit test for simple App.
- */
-public class FizzBuzzTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz(1,16);
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void divisibleByTest() {
+        assertTrue(fizzBuzz.divisibleBy(2,1));
+        assertFalse(fizzBuzz.divisibleBy(3,2));
+    }
+
+    @Test
+    public void fizzBuzzIteratorTest() {
+        List<String> minFizzBuzzList = List.of("1","2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz");
+        assertEquals(minFizzBuzzList, fizzBuzz.getFizzBuzzList());
     }
 }
